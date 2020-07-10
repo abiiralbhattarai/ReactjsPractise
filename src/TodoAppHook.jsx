a80febf65a1eb34b4d6c0408e5203d6160fd89ce
Abiral Bhattarai <bhattarai.abi07@gmail.com>
2020-07-10 15:52:52 +0545

    To do app by using Hooks

--- src/TodoAppHook.jsx
+++ src/TodoAppHook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Toggle() {
   const [isToggleOn, setIsToggleOn] = useState(true);
@@ -13,21 +13,55 @@ function Toggle() {
 }
 
 function TodoAppHook() {
-  const[items, setItems] = useState([]);
-  const[text, setText] = useState("");
+  const [items, setItems] = useState([
+    {
+      id: " 1",
+      text: "Abiral",
+    },
+    {
+      id: " 2",
+      text: "Asim",
+    },
+  ]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e, items, setItems, input, setInput) => {
+    e.preventDefault();
+    const newTodos = [...items, { id: Date.now(), text: input }];
+    setItems(newTodos);
+    setInput("");
+  };
+
+  const deleteItem = (index) => {
+    const updateditems = items.filter((items) => items.id !== index);
+    setItems(updateditems);
+  };
+
   return (
     <div>
       <Toggle />
       <h3>TODO</h3>
       <div>
-      <ul>
-      <li>
-      {}
-      
-      </li>
-      </ul>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.text}
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
+      <form onSubmit={(e) => handleSubmit(e, items, setItems, input, setInput)}>
+        <label htmlFor="new-todo">What needs to be done?</label>
+        <input
+          id="new-todo"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 }
+
 export default TodoAppHook;
